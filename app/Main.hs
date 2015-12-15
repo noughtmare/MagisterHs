e67b11f3a80ab5946103db33a96b5e2d9bbb9711
Noughtmare <noughtmare@openmailbox.org>
2015-12-15 11:05:08 +0100

    Started using lenses to traverse JSON.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Lib
 
 import Control.Error.Util
@@ -8,19 +10,23 @@ import System.IO
 import Control.Exception
 import System.Console.Haskeline
 import Data.Maybe
+import Data.Aeson.Lens
+import Lens.Family2
+import Data.Function (fix)
 
 main :: IO ()
 main = do 
     (school,naam,ww) <- runInputT defaultSettings $ do
         school <- BLIO.pack . fromMaybe "" <$> getInputLine "School: "
-        naam <- BLIO.pack . fromMaybe "" <$> getInputLine "School: "
+        naam <- BLIO.pack . fromMaybe "" <$> getInputLine "Gebruikersnaam: "
         ww <- BLIO.pack . fromMaybe "" <$> getPassword (Just '*') "Wachtwoord: "
         return (school,naam,ww)
-    login naam ww
-    
-login :: BL.ByteString -> BL.ByteString -> IO ()
-login naam ww =
-    either putStrLn (BLIO.putStrLn . prettifyJSON . responseBody) 
-        =<< (\jar -> getHttp (hush jar) (apiUrl ++ "account"))
-        =<< magisterLogin (toLogin naam ww)
+    login naam ww 
 
+login :: BL.ByteString -> BL.ByteString -> IO ()
+login naam ww = magisterLogin (toLogin naam ww) >>=
+    (\jar -> getHttp (hush jar) (apiUrl ++ "account")) >>=
+    either putStrLn (print . fromJust . findId . responseBody) 
+
+findId :: AsValue a => a -> Maybe Integer
+findId inputJSON = inputJSON ^? key "Persoon" . key "Id" . _Integer 
